ef4fe564b4376906bd451cdd7b6bcde1eb0d0b91
Tushar Desarda <tushardesarda@outlook.com>
2024-12-06 00:56:18 +0530

    project setup

---
+++ src/main/java/org/factor_investing/quant_strategy/QuantStrategyApplication.java
@@ -0,0 +1,13 @@
+package org.factor_investing.quant_strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantStrategyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantStrategyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/factor_investing/quant_strategy/momentum/weekly_rebalence/model/WR_StockMomentum.java
@@ -0,0 +1,4 @@
+package org.factor_investing.quant_strategy.momentum.weekly_rebalence.model;
+
+public class WR_StockMomentum {
+}
