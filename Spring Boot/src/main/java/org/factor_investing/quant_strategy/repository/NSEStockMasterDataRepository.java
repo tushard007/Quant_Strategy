ba068178866499585c8692e7ccf73bf6f947d341
Tushar Desarda <tushardesarda@outlook.com>
2025-12-06 21:03:37 +0530

    POM file updated

---
+++ Spring Boot/src/main/java/org/factor_investing/quant_strategy/repository/NSEStockMasterDataRepository.java
@@ -0,0 +1,14 @@
+package org.factor_investing.quant_strategy.repository;
+
+import org.factor_investing.quant_strategy.model.NSEStockMasterData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NSEStockMasterDataRepository extends JpaRepository<NSEStockMasterData, Long> {
+
+
+
+}
